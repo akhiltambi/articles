3e06330b26f95e86d3f2daca65375959a0c56c37
akhiltambi <akhiltambi@gmail.com>
2015-02-24 00:16:10 +0000

    Production Changes

--- config/env/production.js
+++ config/env/production.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports = {
-	db: {
-		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
-		options: {
-			user: '',
-			pass: ''
-		}
-	},
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://mean:password@ds051990.mongolab.com:51990/articledb',
+
+	//{
+	//	uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+	//	options: {
+	//		user: '',
+	//		pass: ''
+	//	}
+	//},
 	log: {
 		// Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
 		format: 'combined',
